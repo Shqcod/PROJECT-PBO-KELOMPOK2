64ab6e148d14c59ef526d844cea63b23a06f98d4
Muhammad Faruqi <165636138+mfaruqi35@users.noreply.github.com>
2024-11-15 10:42:04 +0700

    Update LoginInterface

--- LoginInterface.java
+++ LoginInterface.java
@@ -20,6 +20,27 @@ public class LoginInterface extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new GridBagLayout());
 
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(10,10,10,10);
+        
+        JPanel containerPanel = new JPanel();
+        containerPanel.setLayout(new BoxLayout(containerPanel, BoxLayout.Y_AXIS));
+
+        JLabel headerLabel = new JLabel("Selamat Datang", JLabel.CENTER);
+        headerLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        headerLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        headerLabel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
+
+        containerPanel.add(headerLabel);
+        containerPanel.add(Box.createRigidArea(new Dimension(0, 20))); 
+
+        // gbc.gridx = 0;
+        // gbc.gridy = 0;
+        // gbc.gridwidth = 1;
+        // gbc.anchor = GridBagConstraints.NORTH;
+        // add(headerLabel, gbc);
+
         JPanel mainPanel = new JPanel();
         mainPanel.setPreferredSize(new Dimension(300, 400));
         mainPanel.setBackground(Color.WHITE);
@@ -29,6 +50,7 @@ public class LoginInterface extends JFrame {
             BorderFactory.createEmptyBorder(20, 20, 20, 20)
         ));
 
+        //Label di dalam panel
         JLabel titleLabel = new JLabel("Masuk ke Tokopaedi");
         titleLabel.setFont(new Font("Arial", Font.BOLD, 16));
         titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -36,10 +58,16 @@ public class LoginInterface extends JFrame {
 
         mainPanel.add(Box.createRigidArea(new Dimension(0, 20)));
 
+        String[] roles = {"User", "Admin"};
+        JComboBox<String> roleComboBox = new JComboBox<>(roles);
+        roleComboBox.setMaximumSize(new Dimension(200, 30));
+        roleComboBox.setAlignmentX(Component.CENTER_ALIGNMENT);
+        mainPanel.add(roleComboBox);
+
         // INPUT EMAIL
         JTextField emailField = new JTextField("Masukkan email");
-        emailField.setPreferredSize(new Dimension(200,40));
-        emailField.setMaximumSize(new Dimension(200, 40));
+        emailField.setPreferredSize(new Dimension(200,35));
+        emailField.setMaximumSize(new Dimension(200, 35));
         emailField.setForeground(Color.GRAY);
         emailField.setFont(new Font("Arial", Font.ITALIC, 12));
         emailField.setBorder(BorderFactory.createCompoundBorder(
@@ -72,8 +100,8 @@ public class LoginInterface extends JFrame {
 
         // INPUT PASSWORD
         JTextField passwordField = new JTextField("Masukkan Password");
-        passwordField.setPreferredSize(new Dimension(200, 40));
-        passwordField.setMaximumSize(new Dimension(200, 40));
+        passwordField.setPreferredSize(new Dimension(200, 35));
+        passwordField.setMaximumSize(new Dimension(200, 35));
         passwordField.setForeground(Color.GRAY);
         passwordField.setFont(new Font("Arial", Font.ITALIC, 12));
         passwordField.setBorder(BorderFactory.createCompoundBorder(
@@ -103,14 +131,38 @@ public class LoginInterface extends JFrame {
         });
         
 
-
-
         //emailField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
         mainPanel.add(emailField);
         mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
         mainPanel.add(passwordField);
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
 
-        add(mainPanel);
+        //Tombol login
+        JButton loginButton = new JButton("Login");
+        loginButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        loginButton.setPreferredSize(new Dimension(200, 30));
+        loginButton.setMaximumSize(new Dimension(200, 30));
+        mainPanel.add(loginButton);
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        JLabel noAccountLabel = new JLabel("Belum punya akun?");
+        noAccountLabel.setFont(new Font("Arial", Font.PLAIN, 10));
+        noAccountLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        mainPanel.add(noAccountLabel);
+        mainPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        //Tombol Register
+        JButton registerButton = new JButton("Daftar");
+        registerButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        mainPanel.add(registerButton);
+        
+        containerPanel.add(mainPanel);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.anchor = GridBagConstraints.NORTH;
+        add(containerPanel, gbc);
 
         setFocusable(true);
         requestFocusInWindow();
