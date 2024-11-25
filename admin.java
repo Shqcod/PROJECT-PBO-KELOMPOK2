bd224dd2a65e40ceb03319a46fe9bbc2ca30f73e
Shaldi Shauqi <162072757+Shqcod@users.noreply.github.com>
2024-11-25 13:01:55 +0700

    Update admin.java

--- admin.java
+++ admin.java
@@ -1,1 +1,21 @@
+public class Admin {
+    private String id;
+    private String username;
+    private String password;
+
+    public Admin(String id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{id='" + id + "'}";
+    }
+}
 
