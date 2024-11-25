3592f16c9208bfe05258c14cf29b26b6cd51e570
Shaldi Shauqi <162072757+Shqcod@users.noreply.github.com>
2024-11-25 13:04:03 +0700

    Create Barang.java

---
+++ Barang.java
@@ -0,0 +1,36 @@
+public class Barang {
+    private String nama;
+    private double harga;
+    private int stok;
+
+    public Barang(String nama, double harga, int stok) {
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+
+    @Override
+    public String toString() {
+        return "Barang{nama='" + nama + "', harga=" + harga + ", stok=" + stok + "}";
+    }
+}
